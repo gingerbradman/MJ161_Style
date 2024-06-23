63ee4a32412344e1a686fb942a298dc8ee83c8f8
Carter Hay <clccmh@gmail.com>
2024-06-23 08:59:46 -0500

    Add close button to settings menu.

---
+++ Assets/Scripts/Audio/VolumeManager.cs
@@ -0,0 +1,20 @@
+using FMOD;
+using FMODUnity;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
