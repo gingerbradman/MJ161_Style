35d0fcf4f523e95412bf25b6c0a54eb50f2f671c
SeaLanternn <haidang9079@gmail.com>
2024-06-24 11:19:58 +1200

    new camera

--- Assets/Scripts/Camera/CameraMoveZone.cs
+++ Assets/Scripts/Camera/CameraMoveZone.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 public class CameraMoveZone : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
 	[SerializeField] Vector2 MoveAmount;
 	[SerializeField] float MoveSpeed = .01f;
+	[SerializeField] Canvas canvas;
+
+	void Update()
+	{
+		Vector3 t = (MoveAmount * canvas.gameObject.GetComponent<RectTransform>().sizeDelta / 2);
+		transform.position = canvas.transform.position + t;
+	}
 
 	public void OnPointerEnter(PointerEventData eventData)
 	{
